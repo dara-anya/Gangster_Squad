8fa187201deb810c4e8ee9f1e4f2b895c73ebb48
Dara Anya <daraanya@gmail.com>
2019-03-28 20:39:16 -0500

    ecommerce schema and seed

---
+++ ecommerce_db/seed.sql
@@ -0,0 +1,10 @@
+USE ecommerce;
+
+
+INSERT INTO Posts (title, price, category)
+VALUES 
+	("Lemon Raspberry", 5.20, "/images/Lemon-Raspberry.jpg"), 
+	("Mocha Chocolate", 3.45, "/images/Chocolate-Chocolate.jpg"), 
+    ("Coconut", 4.75, "/images/Coconut.jpg"), 
+    ("Red Velvet", 5.50, "/images/Red-Velvet.jpg"),
+	("Vanilla", 3.25, "/images/Vanilla.jpg");
