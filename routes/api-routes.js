f4656e2e5e0c952c991b5e8db8a7660e1c660d0e
Pedro Magalhães <pedromagalhaes@pedros-mbp.lan>
2019-03-29 19:50:21 -0500

    last version

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -11,38 +11,38 @@ var db = require("../models");
 // Routes
 // =============================================================
 module.exports = function(app) {
-  // GET route for getting all of the posts
-  app.get("/api/posts", function(req, res) {
-    // Add sequelize code to find all posts, and return them to the user with res.json
-    db.Post.findAll({}).then(function(posts) {
-      res.json(posts);
+
+  app.get("/api/products", function(req, res) {
+
+    db.Product.findAll({}).then(function(products) {
+      res.json(products);
     });
   });
 
-  // Get route for returning posts of a specific category
-  app.get("/api/posts/category/:category", function(req, res) {
-    // Add sequelize code to find all posts where the category is equal to req.params.category,
-    db.Post.findAll({
+
+  app.get("/api/products/category/:category", function(req, res) {
+    
+    db.Product.findAll({
       where: {
         category: req.params.category
       }
-      // return the result to the user with res.json
-    }).then(function(posts) {
-      res.json(posts);
+
+    }).then(function(products) {
+      res.json(products);
     });
   });
 
-  // Get route for retrieving a single post
-  app.get("/api/posts/:id", function(req, res) {
-    // Add sequelize code to find a single post where the id is equal to req.params.id,
-    db.Post.findOne({
+
+  app.get("/api/products/:id", function(req, res) {
+
+    db.Product.findOne({
       where: {
         id: req.params.id
       }
     })
       // return the result to the user with res.json
-      .then(function(post) {
-        res.json(post);
+      .then(function(product) {
+        res.json(product);
       });
   });
 
