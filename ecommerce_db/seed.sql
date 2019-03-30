af400d8b6df411100efac9ea4aa0044cead658b5
Dara Anya <daraanya@gmail.com>
2019-03-30 13:07:24 -0500

    edited deleted some products from seed, deleted extra image file, added favIcon

--- ecommerce_db/seed.sql
+++ ecommerce_db/seed.sql
@@ -11,11 +11,4 @@ VALUES
     ("Chocolate Coconut", 5.25, "/images/Chocolate-Coconut.jpg"),
     ("Cinnamon Cream Cheese", 3.25, "/images/Cinnamon-Cream-Cheese.jpg"),
     ("Cookies & Cream", 7.25, "/images/Cookies-And-Cream.jpg"),
-    ("Lemon Meringue", 4.75, "/images/Lemon-Meringue.jpg"),
-    ("Chocolate Peanut Butter", 5.00, "/images/Chocolate-Peanut-Butter.jpg"),
-    ("Chocolate Raspberry", 6.25, "/images/Chocolate-Raspberry.jpg"),
-    ("Lemon Raspberry", 5.25, "/images/Lemon-Raspberry.jpg"),
-    ("Vanilla Milk Chocolate", 4.55, "/images/Vanilla-Milk-Chocolate.jpg"),
-    ("Chocolate Fleur De Sel", 7.19, "/images/Chocolate-Fleur-De-Sel.jpg"),
-    ("Vanilla Strawberry", 4.30, "/images/Vanilla-Strawberry.jpg"),
-    ("Chocolate Gnache", 5.75, "/images/Chocolate-Gnache.jpg");
+    ("Lemon Meringue", 4.75, "/images/Lemon-Meringue.jpg");
